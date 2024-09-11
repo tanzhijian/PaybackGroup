30e99706062dadc141a6d4f9531861f3dd80a5f4
tanzhijian <i@tanzhijian.org>
2024-09-11 22:38:42 +0800

    update

--- PaybackSort.lua
+++ PaybackSort.lua
@@ -1,13 +1,12 @@
 local function HideFirstRaidFrame()
-    C_Timer.After(5, function()
+    C_Timer.After(1, function()
         if CompactRaidFrame1 then
             CompactRaidFrame1:Hide()
-        else
-            print("CompactRaidFrame1 not found")
         end
     end)
 end
 
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("GROUP_ROSTER_UPDATE")
 frame:SetScript("OnEvent", HideFirstRaidFrame)
